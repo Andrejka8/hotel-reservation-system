d7efb7b9111bd03c70cf9d93ccbc5fb3c45bc344
Andrejka8 <andrejka-dvorakova@seznam.cz>
2026-04-26 14:55:50 +0200

    Navbar

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>AD Hotel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <style>
         *{
             font-family: 'Poppins', sans-serif;
@@ -13,9 +14,55 @@
         .h-font{
             font-family: 'Merienda', cursive;
         }
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+        }
+
+        input[type=number] {
+        -moz-appearance: textfield;
+        }
     </style>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">AD Hotel</a>
+            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active me-2" aria-current="page" href="#">Domovská stránka</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="#">Pokoje</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="#">Vybavení</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2" href="#">Kontaktuje nás</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">O nás</a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+                        Přihlásit se
+                    </button>
+                     <button type="button" class="btn btn-outline-dark shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#registerModal">
+                        Registrovat se
+                    </button>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>

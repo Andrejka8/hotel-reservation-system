0acf512177e29b4029acd16a6a12096eee16a285
Andrejka8 <andrejka-dvorakova@seznam.cz>
2026-04-26 21:53:54 +0200

    Features page - stránka o vybavení

--- inc/footer.php
+++ inc/footer.php
@@ -13,9 +13,9 @@
             </div>
             <div class="col-lg-4 p-4 text-center">
                 <h5 class="mb-3">Odkazy</h5>
-                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Domovská stránka</a> <br>
+                <a href="index.php" class="d-inline-block mb-2 text-dark text-decoration-none">Domovská stránka</a> <br>
                 <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Nabídka pokojů</a> <br>
-                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Nabídka vybavení</a> <br>
+                <a href="facilities.php" class="d-inline-block mb-2 text-dark text-decoration-none">Nabídka vybavení</a> <br>
                 <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Kontaktujte nás</a> <br>
                 <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">O nás</a>
             </div>
@@ -35,7 +35,7 @@
             </div>
         </div>
     </div>
-</di>
+</div>
 <h6 class="text-center bg-dark text-white p-3 m-0">Vytvořila Andrea Dvořáková</h6>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
--- inc/header.php
+++ inc/header.php
@@ -8,13 +8,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active me-2" aria-current="page" href="#">Domovská stránka</a>
+                    <a class="nav-link active me-2" aria-current="page" href="index.php">Domovská stránka</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link me-2" href="#">Pokoje</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link me-2" href="#">Vybavení</a>
+                    <a class="nav-link me-2" href="facilities.php">Vybavení</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link me-2" href="#">Kontaktuje nás</a>

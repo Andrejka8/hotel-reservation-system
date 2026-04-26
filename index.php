4c8c549831fa1b9dacf6835fbe77a8fa17a796c7
Andrejka8 <andrejka-dvorakova@seznam.cz>
2026-04-26 19:19:27 +0200

    Testimonials - recenze

--- index.php
+++ index.php
@@ -419,7 +419,107 @@
         </div>
     </div>
 
-    
+    <!-- Testimonials -->
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Recenze</h2>
+    <div class="container">
+        <div class="swiper swiper-testimonials">
+            <div class="swiper-wrapper mb-5">
+                
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/about/rating.svg" width="30px">
+                        <h6 class="m-0 ms-2">Pepa z Brna</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Iusto nihil, esse eum accusamus atque cum veritatis sunt 
+                        laborum, dolor voluptatum soluta maiores et, voluptas 
+                        fuga a incidunt consequatur voluptate at.
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/about/rating.svg" width="30px">
+                        <h6 class="m-0 ms-2">Pepa z Brna</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Iusto nihil, esse eum accusamus atque cum veritatis sunt 
+                        laborum, dolor voluptatum soluta maiores et, voluptas 
+                        fuga a incidunt consequatur voluptate at.
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/about/rating.svg" width="30px">
+                        <h6 class="m-0 ms-2">Pepa z Brna</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Iusto nihil, esse eum accusamus atque cum veritatis sunt 
+                        laborum, dolor voluptatum soluta maiores et, voluptas 
+                        fuga a incidunt consequatur voluptate at.
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/about/rating.svg" width="30px">
+                        <h6 class="m-0 ms-2">Pepa z Brna</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Iusto nihil, esse eum accusamus atque cum veritatis sunt 
+                        laborum, dolor voluptatum soluta maiores et, voluptas 
+                        fuga a incidunt consequatur voluptate at.
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide bg-white p-4">
+                    <div class="profile d-flex align-items-center mb-3">
+                        <img src="images/about/rating.svg" width="30px">
+                        <h6 class="m-0 ms-2">Pepa z Brna</h6>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Iusto nihil, esse eum accusamus atque cum veritatis sunt 
+                        laborum, dolor voluptatum soluta maiores et, voluptas 
+                        fuga a incidunt consequatur voluptate at.
+                    </p>
+                    <div class="rating">
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                        <i class="bi bi-star-fill text-warning"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+
 
 <br><br><br>
 <br><br><br>
@@ -436,6 +536,40 @@
             autoplay: {
                 delay: 3500,
                 disableOnInteraction: false,
+            }
+        });
+
+        var swiper = new Swiper(".swiper-testimonials", 
+        {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            slidesPerView: "3",
+            loop: true,
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                640: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
             }
         });
     </script>

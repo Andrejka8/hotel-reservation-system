842b7149348479ab956335251d280f0cff307a76
Andrejka8 <andrejka-dvorakova@seznam.cz>
2026-04-29 22:02:15 +0200

    Close button in settings - function - funkce pro tlačítko na zavření u nastavení

---
+++ admin/ajax/settings_crud.php
@@ -0,0 +1,15 @@
+<?php
+    require('../inc/db_config.php');
+    require('../inc/essentials.php');
+    adminLogin();
+
+    if(isset($_POST['get_general']))
+    {
+        $q = "SELECT * FROM `settings` WHERE `sr_no`=?";
+        $value = [1];
+        $res = select($q, $value, "i");
+        $data = mysqli_fetch_assoc($res);
+        $json_data = json_encode($data);
+        echo $json_data;
+    }
+?>
--- admin/settings.php
+++ admin/settings.php
@@ -21,7 +21,7 @@
                 <h3 class="mb-4">NASTAVENÍ</h3>
 
                 <!-- General settings section-->
-                 <div class="card">
+                <div class="card">
                     <div class="card-body">
                         <div class="d-flex align-items-center justify-content-between mb-3">
                             <h5 class="card-title m-0">Obecná nastavení</h5>
@@ -30,12 +30,13 @@
                             </button>
                         </div>
                         <h6 class="card-subtitle mb-1 fw-bold">Název stránky</h6>
-                        <p class="card-text">obsah</p>
+                        <p class="card-text" id="site_title"></p>
                         <h6 class="card-subtitle mb-1 fw-bold">O nás</h6>
-                        <p class="card-text">obsah</p>
+                        <p class="card-text" id="site_about"></p>
                     </div>
                 </div>
 
+                <!-- General settings modal -->
                 <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog">
                         <form>
@@ -46,16 +47,16 @@
                                 <div class="modal-body">
                                     <div class="mb-3">
                                         <label class="form-label">Název stránky</label>
-                                        <input type="text" name="site_title" class="form-control shadow-none">
+                                        <input type="text" name="site_title" id="site_title_inp" class="form-control shadow-none">
                                     </div>
                                      <div class="mb-3">
                                         <label class="form-label">O nás</label>
-                                        <textarea name="site_about" class="form-control shadow-none" rows="6"></textarea>
+                                        <textarea name="site_about" id="site_about_inp" class="form-control shadow-none" rows="6"></textarea>
                                     </div>
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn text-secondary shadow-none" data-bs-dismiss="modal">Zavřit</button>
-                                    <button type="button" class="btn custom-bg text-white shadow-none">Potvrdit</button>
+                                    <button type="button" onclick="site_title.value = general.data.site_title, site_about.value = general_data.site_about" class="btn text-secondary shadow-none" data-bs-dismiss="modal">Zavřit</button>
+                                    <button type="button" onclick="upd_general(site_title.value, site_about.value)" class="btn custom-bg text-white shadow-none">Potvrdit</button>
                                 </div>
                             </div>
                         </form>
@@ -66,5 +67,43 @@
     </div>
 
     <?php require('inc/scripts.php');?>
+
+    <script>
+        let general_data;
+
+        function get_general()
+        {
+            let site_title = document.getElementById('site_title');
+            let site_about = document.getElementById('site_about');
+
+            let site_title_inp = document.getElementById('site_title_inp');
+            let site_about_inp = document.getElementById('site_about_inp');
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/settings_crud.php", true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+            xhr.onload = function()
+            {
+                general_data = JSON.parse(this.responseText);
+
+                site_title.innerText = general_data.site_title;
+                site_about.innerText = general_data.site_about;
+
+                site_title_inp.value = general_data.site_title;
+                site_about_inp.value = general_data.site_about;
+
+            }
+            
+
+            xhr.send('get_general');
+        }
+
+        
+        window.onload = function()
+        {
+            get_general();
+        }
+    </script>
 </body>
 </html>
